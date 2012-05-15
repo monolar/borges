25e6f3816af990bdb2ad98ccd0bd27ab4ef0aacb
Andreas Marr <andreasm@artcom.de>
2012-05-15 12:17:03 +0200

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Borges::Application.config.secret_token = '9bb80aa47d68a165452d1c72e4071fabab69a87a8d0c06985d1d528bcfb6f99f9dda7bf6261c2fa52f5d437d3f77d8be77fdf70d32ec1633a59c110c51130bee'
